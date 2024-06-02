13226ab34e003d61fe3b7eb2db3f96026e40d5cd
Sikan777 <sikanbog@gmail.com>
2024-06-02 18:43:01 +0300

    sructurenahyj

--- PoopOk/news/views.py
+++ PoopOk/news/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'news/post_list.html', {'posts': posts})
+
+@login_required
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        comment_form = CommentForm()
+    return render(request, 'news/post_detail.html', {'post': post, 'comment_form': comment_form})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'news/create_post.html', {'form': form})
