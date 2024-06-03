4f844e2645093738a817e9f5e489eaa2dd9e4f8a
Barskyi <barskyi69@gmail.com>
2024-06-03 12:50:33 +0300

    refacoring
    add .idea into gitignore and delete comment in sett

--- poopok/poopok/settings.py
+++ poopok/poopok/settings.py
@@ -71,16 +71,6 @@ WSGI_APPLICATION = 'poopok.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': (os.getenv('NAME')),
-#         'USER': (os.getenv('USER')),
-#         'PASSWORD': (os.getenv('PASSWORD')),
-#         'HOST': (os.getenv('HOST')),
-#         'PORT': (os.getenv('PORT')),
-#     }
-# }
 
 DATABASES = {
     'default': {
